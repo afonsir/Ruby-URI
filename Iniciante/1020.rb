2fe8aa562ba9e1d8a11162006a44bad82457fe97
afonsir <afonsir17@gmail.com>
2017-11-02 19:26:12 -0200

    Idade em Dias

---
+++ Iniciante/1020.rb
@@ -0,0 +1,11 @@
+idade_dias = gets.strip.to_i
+
+idade_anos = idade_dias / 365
+idade_dias %= 365
+
+idade_meses = idade_dias / 30
+idade_dias %= 30
+
+puts "#{idade_anos} ano(s)"
+puts "#{idade_meses} mes(es)"
+puts "#{idade_dias} dia(s)"
