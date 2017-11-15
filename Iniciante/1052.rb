8cbd4428338bea0f4482c6f7622f0405baee7cd0
afonsir <afonsir17@gmail.com>
2017-11-15 14:17:10 -0200

    1052 - Mês

---
+++ Iniciante/1052.rb
@@ -0,0 +1,5 @@
+mes = gets.strip.to_i
+
+meses = ["", "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+puts "#{meses[mes]}"
