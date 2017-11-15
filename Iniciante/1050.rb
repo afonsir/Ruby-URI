92efede15ddf080c96bf419f39a92042f395b50a
afonsir <afonsir17@gmail.com>
2017-11-15 13:34:49 -0200

    1050 - DDD

---
+++ Iniciante/1050.rb
@@ -0,0 +1,13 @@
+codigo = gets.strip.to_i
+
+case codigo
+when 61 then puts "Brasilia"
+when 71 then puts "Salvador"
+when 11 then puts "Sao Paulo"
+when 21 then puts "Rio de Janeiro"
+when 32 then puts "Juiz de Fora"
+when 19 then puts "Campinas"
+when 27 then puts "Vitoria"
+when 31 then puts "Belo Horizonte"
+else puts "DDD nao cadastrado"
+end
